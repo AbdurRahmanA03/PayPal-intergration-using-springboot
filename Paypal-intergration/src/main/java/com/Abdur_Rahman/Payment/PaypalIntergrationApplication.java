02b1aa6a5c3cf89c4e85c70eaf420cff41a46cc6
AbdurRahmanA03 <Abdur-RahmanAli@outlook.com>
2024-08-04 22:37:25 -0400

    Add files via upload
    Has issues running locally.

---
+++ Paypal-intergration/src/main/java/com/Abdur_Rahman/Payment/PaypalIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.Abdur_Rahman.Payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalIntergrationApplication.class, args);
+	}
+
+}
---
+++ Paypal-intergration/src/test/java/com/Abdur_Rahman/Payment/PaypalIntergrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abdur_Rahman.Payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntergrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
